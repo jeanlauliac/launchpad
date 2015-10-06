4f15d643435e97a668e02d1e0a926f7a0074acae
Jean Lauliac <jean@lauliac.com>
2015-10-07 00:26:35 +0100

    Particles!

--- source/index.js
+++ source/index.js
@@ -6,15 +6,36 @@ function main() {
   const driver = new LaunchpadDriver(0)
   const buffer = new LaunchpadBuffer(driver)
 
-  const particules = []
+  let particules = []
+
+  let oneLoop = () => {
+    buffer.clear()
+    particules.forEach(particule => {
+      buffer.set(particule.position, particule.color)
+    })
+    buffer.present()
+    particules = particules.map(particule => ({
+      position: {
+        x: particule.position.x + particule.velocity.x,
+        y: particule.position.y + particule.velocity.y,
+      },
+      color: particule.color,
+      velocity: particule.velocity,
+    })).filter(particule => (
+      particule.position.y < 8 && particule.position.y >= 0 &&
+      particule.position.x < 9 && particule.position.x >= 0
+    ))
+  }
 
   driver.on('cellPress', position => {
-    buffer.set(position, {red: 3, green: 0})
-    buffer.present()
-  }).on('cellRelease', position => {
-    buffer.set(position, {red: 0, green: 0})
-    buffer.present()
+    const color = {red: 3, green: 1}
+    particules.push({position, color, velocity: {x: 1, y: 0}})
+    particules.push({position, color, velocity: {x: -1, y: 0}})
+    particules.push({position, color, velocity: {x: 0, y: 1}})
+    particules.push({position, color, velocity: {x: 0, y: -1}})
   })
+
+  setInterval(oneLoop, 50)
 
 }
 
